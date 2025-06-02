582a8c9e87199fe6024a9bd9361305dde9e4e5ba
piersto <44554156+piersto@users.noreply.github.com>
2025-06-02 15:18:32 -0400

    Started shirt.py

--- test/test_06/shirt/shirt.py
+++ test/test_06/shirt/shirt.py
@@ -9,39 +9,50 @@ def main():
             sys.exit("Too few command-line arguments")
         elif len(sys.argv) > 3:
             sys.exit("Too many command-line arguments")
-        before = sys.argv[1].strip().lower()
-        after = sys.argv[2].strip().lower()
+        before = sys.argv[1]
+        after = sys.argv[2]
 
-        # before = "before.jpeg"
+        # before = "before.jpg"
         # after = "after.jpg"
 
-        if not before.endswith(".png") \
-                and before.endswith(".jpg") \
-                and before.endswith(".jpeg"):
+        if before.endswith(".png"):
+            pass
+        elif before.endswith(".jpg"):
+            pass
+        elif before.endswith(".jpeg"):
+            pass
+        else:
             sys.exit("Invalid input")
 
-        elif not after.endswith(".png") \
-                and not after.endswith(".jpg") \
-                and not after.endswith(".jpeg"):
+        if after.endswith(".png"):
+            pass
+        elif after.endswith(".jpg"):
+            pass
+        elif after.endswith(".jpeg"):
+            pass
+        else:
             sys.exit("Invalid input")
 
-        if before.endswith(".png") and not after.endswith(".png") \
-                or before.endswith(".jpg") and not after.endswith(".jpg") \
-                or before.endswith(".jpeg") and not after.endswith(".jpeg"):
+        if before.endswith(".png") and not after.endswith(".png"):
+            sys.exit("Input and output have different extensions")
+        elif before.endswith(".jpg") and not after.endswith(".jpg"):
+            sys.exit("Input and output have different extensions")
+        elif before.endswith(".jpeg") and not after.endswith(".jpeg"):
             sys.exit("Input and output have different extensions")
         else:
             # shirt = Image.open("C:\PROJECTS\PYTHON\CX50P\shirt.png")
-            size = (1200, 1600)
+            # size = (1200, 1600)
 
             with Image.open(before) as img:
                 after_resized = ImageOps.fit(img, (600, 600))
-                after_resized.save("apter_resized.jpg")
+                after_resized.save("after_resized.jpg")
 
-            with Image.open("shirt.png") as s_i, Image.open("after_resized") as a_r:
-                Image.Image.paste(s_i, a_r)
+            with Image.open(r"C:\PROJECTS\PYTHON\CX50P\shirt.png") as s_i, \
+                    Image.open(r"C:\PROJECTS\PYTHON\CX50P\after_resized.jpg") as a_r:
+                Image.Image.paste(s_i, a_r, mask=s_i)
 
                 after_resized.save("out.jpg")
-                print(after_resized.size)
+                # print(after_resized.size)
 
     except FileNotFoundError:
         sys.exit("File not found")
