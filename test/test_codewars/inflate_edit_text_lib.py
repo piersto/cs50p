94f9e645d9fe2cf2ca25cf8bb6d925669092a39d
piersto <44554156+piersto@users.noreply.github.com>
2025-06-13 23:55:29 -0400

    Codewar kata using inflect

---
+++ test/test_codewars/inflate_edit_text_lib.py
@@ -0,0 +1,56 @@
+import inflect
+
+def calculate_age(year_of_birth, current_year):
+
+    p = inflect.engine()
+    dif = int(year_of_birth) - int(current_year)
+
+    if dif == 0:
+        return "You were born this very year!"
+    elif dif < 0:
+        dif = str(abs(dif))
+        return f"You are {dif} {p.plural_noun('year', dif)} old."
+    elif dif > 0:
+        dif = str(abs(dif))
+        return f"You will be born in {dif} {p.plural_noun('year', dif)}."
+
+
+# from .test_test import calculate_age
+
+
+def test_test():
+
+    a = "2012"
+    b = "2012"
+
+    assert calculate_age(a, b ) == "You were born this very year!"
+
+
+def test_test_2():
+
+    a = "900"
+    b = "2900"
+
+    assert calculate_age(a, b ) == "You are 2000 years old."
+
+
+def test_test_3():
+
+    a = "2900"
+    b = "900"
+
+    assert calculate_age(a, b ) == "You will be born in 2000 years."
+
+def test_test_4():
+
+    a = "2000"
+    b = "2001"
+
+    assert calculate_age(a, b ) == "You are 1 year old."
+
+def test_test_5():
+
+    a = "2001"
+    b = "2000"
+
+    assert calculate_age(a, b ) == "You will be born in 1 year."
